7f4a0e4b8b4330bd5f9f49b19e1a3782ec2732c6
liz.gv <lizzy.gv.2006@gmail.com>
2026-08-17 11:08:27 -0300

    Create ddl_bronze.sql

---
+++ scrips/bronze/ddl_bronze.sql
@@ -0,0 +1,66 @@
+/* 
+========================================================================================================================
+DDL SCRIPT: Creando las tablas de la capa bronze
+========================================================================================================================
+Proposito del script: 
+  El script genera tablas en el esquema 'bronze', eliminando tablas existentes con el mismo nombre antes de su ejecución.
+  Se usa para redefinir la estructura DDL de las tablas del esquema 'bronze'.
+=========================================================================================================================
+*/
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	csr_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_material_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_prod_info (
+prd_id INT,
+prd_key VARCHAR(50),
+prd_nm VARCHAR(50),
+prd_cost NUMERIC,
+prd_line CHAR(1),
+prd_start_dt DATE,
+prd_end_dt DATE
+);
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_sales_details (
+sls_ord_num VARCHAR(50),
+sls_prd_key VARCHAR(50),
+sls_cust_id INT,
+sls_order_dit INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.erp_cust_az12(
+cid VARCHAR(50),
+bdate DATE,
+GEN VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.erp_loc_a101 (
+cid VARCHAR(50),
+cntry VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id VARCHAR(50),
+cat VARCHAR(50),
+subcat VARCHAR(50),
+maintenance VARCHAR(50)
+);
+
