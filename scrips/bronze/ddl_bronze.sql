d80e8ee63522a6957e011d4996b63b778179cbbb
liz.gv <lizzy.gv.2006@gmail.com>
2026-08-19 09:27:59 -0300

    Update ddl_bronze.sql

--- scrips/bronze/ddl_bronze.sql
+++ scrips/bronze/ddl_bronze.sql
@@ -35,7 +35,7 @@ CREATE TABLE bronze.crm_sales_details (
 sls_ord_num VARCHAR(50),
 sls_prd_key VARCHAR(50),
 sls_cust_id INT,
-sls_order_dit INT,
+sls_order_dt INT,
 sls_ship_dt INT,
 sls_due_dt INT,
 sls_sales INT,
